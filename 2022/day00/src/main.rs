3957c7d9fbbc4fea33ed7497307c149db66e189c
Mert Dogan <mert.dogan@me.com>
2022-11-30 17:27:00 +0100

    day00 (template) changed

--- 2022/day00/src/main.rs
+++ 2022/day00/src/main.rs
@@ -4,5 +4,34 @@ fn main() {
     let input: String = fs::read_to_string("input.txt")
         .expect("File input.txt doesn't exist");
 
-    println!("{input}");
+    let result = run(input);
+
+    fs::write("output.txt", result)
+        .expect("Couln't write to file output.txt");
+
+    println!("Output in file output.txt");
+}
+
+fn run(input: String) -> String {
+    input
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fs;
+
+    use crate::run;
+
+    #[test]
+    fn run_test() {
+        let input: String = fs::read_to_string("test-input.txt")
+            .expect("File test-input.txt doesn't exist");
+
+        let expected_output: String = fs::read_to_string("test-output.txt")
+            .expect("File test-output.txt doesn't exist");
+
+        let result = run(input);
+
+        assert_eq!(result, expected_output);
+    }
 }
