d1560f525c0e0a06667fd2b97b86ebbf6093f8a6
Mert Dogan <mert.dogan@me.com>
2022-11-30 15:30:30 +0100

    target removed

--- 2022/day00/target/debug/deps/day00-e8194df3b523a1f2.d
+++
@@ -1,5 +0,0 @@
-/home/mert/gitrepos/aoc/2022/day00/target/debug/deps/day00-e8194df3b523a1f2.rmeta: src/main.rs
-
-/home/mert/gitrepos/aoc/2022/day00/target/debug/deps/day00-e8194df3b523a1f2.d: src/main.rs
-
-src/main.rs:
--- 2022/day00/target/debug/deps/day00-f7829251975a36af.d
+++
@@ -1,5 +0,0 @@
-/home/mert/gitrepos/aoc/2022/day00/target/debug/deps/day00-f7829251975a36af: src/main.rs
-
-/home/mert/gitrepos/aoc/2022/day00/target/debug/deps/day00-f7829251975a36af.d: src/main.rs
-
-src/main.rs:
