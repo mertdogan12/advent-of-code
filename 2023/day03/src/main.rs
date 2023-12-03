83337c0c0ba4bff79c3dd477f470c35e40875d13
Mert Dogan <mert.dogan@me.com>
2023-12-04 00:54:29 +0100

    day03 part 2 finished

--- 2023/day03/src/main.rs
+++ 2023/day03/src/main.rs
@@ -1,9 +1,9 @@
-use std::fs;
+use std::{collections::HashMap, fs};
 
 fn main() {
     let input = fs::read_to_string("./input.txt").expect("Could not read input.txt");
 
-    let output = part1(&input);
+    let output = part2(&input);
     println!("{output}")
 }
 
@@ -62,8 +62,113 @@ fn part1(input: &str) -> String {
     return out.to_string();
 }
 
+fn part2(input: &str) -> String {
+    let mut out = 0;
+    let mut hashmap: HashMap<(usize, usize), [i32; 2]> = HashMap::new();
+    let lines: Vec<String> = input.lines().map(|l| format!(".{l}.")).collect();
+
+    for (i, line) in lines.iter().enumerate() {
+        let mut symbol = false;
+        let mut value = String::new();
+        let mut gearpos = (0, 0); // x, y
+
+        let chars = line.chars();
+        
+        let next_chars = if i >= lines.len() - 1 {
+            "".chars()
+        } else {
+            lines[i + 1].chars()
+        };
+
+        let prev_chars = if i == 0 {
+            "".chars()
+        } else {
+            lines[i - 1].chars()
+        };
+
+        for (j, char) in chars.clone().enumerate() {
+            if char.is_digit(10) {
+                value.push(char);
+
+                if symbol
+                    || is_gear(
+                        chars.clone().nth(j - 1).unwrap_or('.'),
+                        (j - 1, i),
+                        &mut gearpos,
+                    )
+                    || is_gear(
+                        chars.clone().nth(j + 1).unwrap_or('.'),
+                        (j + 1, i),
+                        &mut gearpos,
+                    )
+                    || is_gear(
+                        next_chars.clone().nth(j).unwrap_or('.'),
+                        (j, i + 1),
+                        &mut gearpos,
+                    )
+                    || is_gear(
+                        next_chars.clone().nth(j - 1).unwrap_or('.'),
+                        (j - 1, i + 1),
+                        &mut gearpos,
+                    )
+                    || is_gear(
+                        next_chars.clone().nth(j + 1).unwrap_or('.'),
+                        (j + 1, i + 1),
+                        &mut gearpos,
+                    )
+                    || is_gear(
+                        prev_chars.clone().nth(j).unwrap_or('.'),
+                        (j, i - 1),
+                        &mut gearpos,
+                    )
+                    || is_gear(
+                        prev_chars.clone().nth(j - 1).unwrap_or('.'),
+                        (j - 1, i - 1),
+                        &mut gearpos,
+                    )
+                    || is_gear(
+                        prev_chars.clone().nth(j + 1).unwrap_or('.'),
+                        (j + 1, i - 1),
+                        &mut gearpos,
+                    )
+                {
+                    symbol = true;
+                }
+
+                continue;
+            }
+
+            if symbol {
+                let num = value.parse::<i32>().unwrap();
+
+                if hashmap.contains_key(&gearpos) {
+                    hashmap.get_mut(&gearpos).unwrap()[1] = num;
+                } else {
+                    hashmap.insert(gearpos, [num, 0]);
+                }
+            }
+
+            symbol = false;
+            value = String::new();
+        }
+    }
+
+    hashmap.iter().for_each(|m| out += m.1[0] * m.1[1]);
+
+    return out.to_string();
+}
+
 fn is_symbol(c: char) -> bool {
     return c != '.' && !c.is_digit(10);
+}
+
+fn is_gear<'a>(c: char, pos: (usize, usize), gear: &'a mut (usize, usize)) -> bool {
+    if c == '*' {
+        *gear = pos;
+        return true;
+    }
+
+    return false;
 }
 
 #[cfg(test)]
@@ -79,6 +184,18 @@ mod tests {
             .replace('\n', "");
 
         let output = part1(&input);
+
+        assert_eq!(result, output)
+    }
+
+    #[test]
+    fn part2_test() {
+        let input = fs::read_to_string("./test.txt").expect("Could not read test.txt");
+        let result = fs::read_to_string("./result2.txt")
+            .expect("Could not read result2.txt")
+            .replace('\n', "");
+
+        let output = part2(&input);
 
         assert_eq!(result, output)
     }
