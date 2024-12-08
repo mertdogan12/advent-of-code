130b9c0ad1af8f3dac756d2624f9d0aa86a3246d
Mert Dogan <mert.dogan@me.com>
2024-12-08 17:58:17 +0100

    template changes

---
+++ 2024/template/day.c
@@ -0,0 +1,13 @@
+#include "day.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int part1(FILE* input) {
+
+}
+
+int part2(FILE* input) {
+
+}
--- 2024/template/test.c
+++ 2024/template/test.c
@@ -1,25 +1,23 @@
-#include "main.h"
+#include "day.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 
 int main() {
-    const int correctOutput = 0;
-    FILE* file = fopen("input.txt", "r");
+    const int correctOutput = 31;
+    FILE* file = fopen("test-input.txt", "r");
     if (file == NULL)
         exit(EXIT_FAILURE);
 
-    int result = part1(file);
+    int result = part2(file);
 
     if (result == correctOutput) {
-        printf("Correct result: %d", result);
+        printf("Correct result: %d\n", result);
     } else {
-        printf("Wrong result: %d", result);
+        printf("Wrong result: %d\n", result);
     }
 
     fclose(file);
-    if (file != NULL)
-        free(file);
     exit(EXIT_SUCCESS);
 
     result -1;
