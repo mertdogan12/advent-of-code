1463dc1a92beb4f9d915392c66295241be8b737b
Mert Dogan <mert.dogan@me.com>
2026-09-21 16:23:16 +0200

    2025/day05 part 2 finished

--- 2025/day05/bin/aoc.ml
+++ 2025/day05/bin/aoc.ml
@@ -1,3 +1,5 @@
+let calc x y = y - x + 1;;
+
 let parse_range line =
   let (fst, snd) = Option.get (String.split_first ~sep:"-" line) in
   (int_of_string fst, int_of_string snd)
@@ -10,24 +12,30 @@ let rec parse_ranges ic list =
   | exception End_of_file -> list
 ;;
 
-let rec exists i list =
-  match list with
-  | x :: y -> if i >= fst x && i <= snd x then true else exists i y
-  | [] -> false
+let merge_overlap list =
+  let rec mo list list_out =
+    match list with
+    | x :: y :: z ->
+      if (snd x) >= (snd y) then
+        mo (x :: z) list_out
+      else if (snd x) >= (fst y) then
+        mo ((fst x, snd y) :: z) list_out
+      else
+        mo (y :: z) (x :: list_out)
+    | [x] -> x :: list_out
+    | [] -> list_out
+  in List.rev (mo list [])
 ;;
 
-let rec solve ic out ranges =
-  match int_of_string (input_line ic) with
-  | x ->
-    if exists x ranges then
-      solve ic (out + 1) ranges
-    else
-      solve ic out ranges
-  | exception End_of_file -> out
+let rec solve ranges out =
+  match ranges with
+  | x :: y -> solve y (out + (calc (fst x) (snd x)))
+  | [] -> out
 ;;
 
 let solve ic =
   let us_ranges = parse_ranges ic [] in
-  let ranges = List.sort (fun a b -> Int.compare (fst a) (fst b)) us_ranges in
-  solve ic 0 ranges
+  let um_ranges = List.sort (fun a b -> Int.compare (fst a) (fst b)) us_ranges in
+  let ranges = merge_overlap um_ranges in
+  solve ranges 0
 ;;
